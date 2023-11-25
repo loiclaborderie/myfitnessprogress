adf8cf9a6f53775cb0756b9bec91a79bf4605068
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2023-11-25 22:16:09 +0700

    Add User model and authentication contracts

---
+++ database/migrations/1700919867039_users.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('email').notNullable().unique()
+      table.string('password').notNullable()
+      table.integer('height').notNullable()
+      table.integer('weight').notNullable()
+      table.timestamp('created_at').defaultTo(this.now())
+      table.timestamp('updated_at').defaultTo(this.now())
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
